38321f409886093992e94ff7c1a489cef5c31831
solivanlau <solivan_lau@outlook.com>
2023-03-13 20:46:40 -0400

    added darkmode toggle, adds class to body based on boolean value - need scss theme integration

---
+++ scripts/modules/nav.js
@@ -0,0 +1,53 @@
+// nav system 
+const sectionLinkHTML = (parentElement, href) => {
+    const linkElement = document.createElement('div')
+    linkElement.classList.add('sectionNavBtn')
+    linkElement.innerHTML = `
+        <a href="#${href}">
+            <div class="navBtn">
+                <div class="arrowContainer">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 48 48">
+                        <path d="m22.7 29.25-6.45-6.45q-.9-.9-.425-2.075.475-1.175 1.775-1.175h12.9q1.2 0 1.7 1.175t-.45 2.075l-6.35 6.45q-.3.25-.65.375-.35.125-.75.125-.35 0-.7-.125t-.6-.375Z"/>
+                    </svg>
+                </div>
+            </div>
+        </a>
+        `
+    parentElement.appendChild(linkElement)
+
+}
+
+const sideNavHTML = (parentElement, href) => {
+    const navIcon = document.createElement('div')
+    navIcon.classList.add('navIcon')
+    navIcon.innerHTML = `
+    <a href="#${href}">
+        <div class="iconContainer">
+            <svg viewBox="0 0 100 100" >
+                <circle cx="50" cy="50" r="50" />
+            </svg>
+        </div>
+    </a>
+    `
+    parentElement.appendChild(navIcon)
+}
+
+const buildNav = (sectionArr, navElement) => {
+    //icon per nav section
+    // sectionArr.forEach(icon => {
+    //     sideNavHTML(navElement, icon.id);
+    // });
+
+    // adding link to next secton UNLESS last 
+    for (let i = 0; i < sectionArr.length - 1; i++) {
+        // opted to use for loop - iterates logic UNTIL last section
+        const nextSectionIndex = (i + 1);
+
+        sectionLinkHTML(sectionArr[i], sectionArr[nextSectionIndex].id)
+    }
+
+    // // functionality
+    // const sideNavbtns = document.getElementsByClassName('')
+}
+
+export default buildNav;
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,64 @@
 import inputLabelHandler from './modules/input.js';
+import buildNav from './modules/nav.js';
 
 
-// event listener: input/label system
+
+
+// input/label system
 document.addEventListener('click', inputLabelHandler)
+
+// dynamic nav system
+const navItems = document.querySelectorAll('.navItem');
+const sideNav = document.getElementById('sideNav');
+
+buildNav(navItems, sideNav);
+
+// theme toggle btn
+const darkModeBtn = document.getElementById('darkModeToggle')
+const toggleIcon = document.getElementById('toggleIcon')
+
+const userThemePreference = window.matchMedia("(prefers-color-scheme: dark)")
+let darkModeStatus = '';
+
+// checking preference on load
+
+const checkUserTheme = () => {
+    if (userThemePreference.matches === true) {
+        console.log(`${userThemePreference.matches}, set toggle button to dark mode`)
+        darkModeStatus = true;
+    } else {
+        console.log(`${userThemePreference.matches}, set toggle button to light mode`)
+        darkModeStatus = false;
+    }
+    applyTheme()
+}
+
+const handleToggleDarkMode = () => {
+    darkModeStatus = !darkModeStatus;
+    console.log(`darkModeStatus is currently ${darkModeStatus}`)
+    applyTheme()
+
+}
+
+const applyTheme = () => {
+    const rootElement = document.body;
+    if (darkModeStatus === true) {
+        rootElement.classList.add('darkTheme');
+        rootElement.classList.remove('lightTheme');
+
+        toggleIcon.classList.add('dark');
+        toggleIcon.classList.remove('light');
+
+        console.log('dark mode')
+    } else {
+        rootElement.classList.add('lightTheme');
+        rootElement.classList.remove('darkTheme');
+
+        toggleIcon.classList.add('light');
+        toggleIcon.classList.remove('dark');
+        console.log('light mode')
+
+    }
+}
+window.onload = checkUserTheme;
+darkModeBtn.addEventListener('click', handleToggleDarkMode)
