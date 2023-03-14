a191b5a9a66c65224bf8fccdfc57d6ff03e8fbe5
solivanlau <solivan_lau@outlook.com>
2023-03-14 08:59:47 -0400

    scss theme map created - linked to js functionality

---
+++ scripts/modules/darkMode.js
@@ -0,0 +1,47 @@
+// theme toggle btn
+const toggleIcon = document.getElementById('toggleIcon')
+
+const userThemePreference = window.matchMedia("(prefers-color-scheme: dark)")
+let darkModeStatus = '';
+
+// applies theme based on darkModeStatus boolean val
+const applyTheme = () => {
+    const rootElement = document.body;
+    if (darkModeStatus === true) {
+        rootElement.classList.add('themeDark');
+        rootElement.classList.remove('themeLight');
+
+        toggleIcon.classList.add('dark');
+        toggleIcon.classList.remove('light');
+
+        console.log('dark mode')
+    } else {
+        rootElement.classList.add('themeLight');
+        rootElement.classList.remove('themeDark');
+
+        toggleIcon.classList.add('light');
+        toggleIcon.classList.remove('dark');
+        console.log('light mode')
+
+    }
+}
+
+
+// checking preference on load
+export const checkUserTheme = () => {
+    if (userThemePreference.matches === true) {
+        console.log(`${userThemePreference.matches}, set toggle button to dark mode`)
+        darkModeStatus = true;
+    } else {
+        console.log(`${userThemePreference.matches}, set toggle button to light mode`)
+        darkModeStatus = false;
+    }
+    applyTheme()
+}
+
+export const handleToggleDarkMode = () => {
+    darkModeStatus = !darkModeStatus;
+    console.log(`darkModeStatus is currently ${darkModeStatus}`)
+    applyTheme()
+}
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,64 +1,20 @@
 import inputLabelHandler from './modules/input.js';
 import buildNav from './modules/nav.js';
+import { checkUserTheme, handleToggleDarkMode } from './modules/darkMode.js'
 
 
-
-
-// input/label system
-document.addEventListener('click', inputLabelHandler)
-
-// dynamic nav system
+// HTML VARIABLES
+// nav
 const navItems = document.querySelectorAll('.navItem');
 const sideNav = document.getElementById('sideNav');
-
-buildNav(navItems, sideNav);
-
-// theme toggle btn
+// darkmode
 const darkModeBtn = document.getElementById('darkModeToggle')
-const toggleIcon = document.getElementById('toggleIcon')
 
-const userThemePreference = window.matchMedia("(prefers-color-scheme: dark)")
-let darkModeStatus = '';
 
-// checking preference on load
-
-const checkUserTheme = () => {
-    if (userThemePreference.matches === true) {
-        console.log(`${userThemePreference.matches}, set toggle button to dark mode`)
-        darkModeStatus = true;
-    } else {
-        console.log(`${userThemePreference.matches}, set toggle button to light mode`)
-        darkModeStatus = false;
-    }
-    applyTheme()
-}
-
-const handleToggleDarkMode = () => {
-    darkModeStatus = !darkModeStatus;
-    console.log(`darkModeStatus is currently ${darkModeStatus}`)
-    applyTheme()
-
-}
-
-const applyTheme = () => {
-    const rootElement = document.body;
-    if (darkModeStatus === true) {
-        rootElement.classList.add('darkTheme');
-        rootElement.classList.remove('lightTheme');
-
-        toggleIcon.classList.add('dark');
-        toggleIcon.classList.remove('light');
-
-        console.log('dark mode')
-    } else {
-        rootElement.classList.add('lightTheme');
-        rootElement.classList.remove('darkTheme');
-
-        toggleIcon.classList.add('light');
-        toggleIcon.classList.remove('dark');
-        console.log('light mode')
-
-    }
-}
 window.onload = checkUserTheme;
+// dynamic nav system
+buildNav(navItems, sideNav);
+// darkMode Toggle
 darkModeBtn.addEventListener('click', handleToggleDarkMode)
+// input/label system
+document.addEventListener('click', inputLabelHandler)
