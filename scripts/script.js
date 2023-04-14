c87855f0b121ab063568e5d14441ed9dc923b5fc
solivanlau <solivan_lau@outlook.com>
2023-04-14 11:08:56 -0400

    mobile menu working. mobile fix made - still need to work on media query for form

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,53 @@
+import mobileToggle from './modules/mobileNav.js';
 import inputLabelHandler from './modules/input.js';
 import buildNav from './modules/nav.js';
 import { checkUserTheme, handleToggleDarkMode } from './modules/darkMode.js'
 
 
 // HTML VARIABLES
-// nav
+
+// mobile nav
+const hamburgerMenu = document.getElementById('mobileMenu')
+const headerNav = document.getElementById('linkContainer')
+
+// nav per section
 const navItems = document.querySelectorAll('.navItem');
 const sideNav = document.getElementById('sideNav');
 // darkmode
 const darkModeBtn = document.getElementById('darkModeToggle')
-
 const contactFrom = document.getElementById('contactForm')
 
 
+//**** PAGE LOAD SET UP ****
+
+// dark/light mode theme
 window.addEventListener("load", () => {
-    // onload dark/light theme preference check
+    // onload USER theme preference check
     checkUserTheme()
     //reset contact form
     contactFrom.reset();
 });
+
 // build nav link per section
 buildNav(navItems, sideNav);
-// darkMode Toggle
 
+//initial check for form inputs
+inputLabelHandler()
+
+//**** INPUT INTERACTIONS ****
+
+// mobile hamburger menu
+hamburgerMenu.addEventListener('click', () => { mobileToggle(headerNav) })
+
+// darkMode Toggle
 darkModeBtn.addEventListener('click', handleToggleDarkMode)
 
 // input/label system
 document.addEventListener('click', inputLabelHandler)
-//initial check
-inputLabelHandler()
+
+
+
+
 // HTML VARIABLES AVATAR RIG 
 // head
 const avatarHairAccent = document.getElementById('hairAccent')
