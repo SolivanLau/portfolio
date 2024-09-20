8997280d394d128d66a7a6e033fa8c83f987436e
solivan-lau <solivan_lau@outlook.com>
2024-09-20 02:13:52 -0400

    CONFIG: big config update to vite. configured css optimizations w postcss and sass embedded replacing sass. removed css folder

---
+++ vite.config.mjs
@@ -0,0 +1,30 @@
+import {resolve} from 'path'
+import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
+export default defineConfig({
+  root: './src', 
+  build: {
+    outDir: '../dist', 
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'src/index.html'),
+        email: resolve(__dirname, 'src/email.html'),
+      }
+    }
+  },
+  server: {
+    open: true 
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      }
+    }
+  },
+  plugins: [
+    createHtmlPlugin({
+      minify: true
+    })
+  ]
+})
